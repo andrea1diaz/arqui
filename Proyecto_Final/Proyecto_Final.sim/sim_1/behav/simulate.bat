61e2783bfd0e4076ad62e37bc742782d51f3124b
Andrea <andrea.diaz.12@me.com>
2018-10-24 09:35:45 -0500

    carpetas en vhdl

--- Proyecto_Final/Proyecto_Final.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xsim MIPS_Top_tb_behav -key {Behavioral:sim_1:Functional:MIPS_Top_tb} -tclbatch MIPS_Top_tb.tcl -view C:/Users/Alessio/Desktop/Proyecto_Final/Proyecto_Final.srcs/sim_1/imports/Proyecto/MIPS_Top_tb_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
