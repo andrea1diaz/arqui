99d91805036d19adeb07f9555ce69ebbe2367047
Andrea <andrea.diaz.12@me.com>
2018-11-04 18:07:29 -0500

    revisar operation 0111 (less than)

--- ALU.v
+++ ALU.v
@@ -3,10 +3,21 @@ module ALU (A, B, zero, ALUresult, ALUcontrol);
   output [31:0]ALUresult;
   input [31:0]A;
   input [31:0]B;
-  input [2:0] ALUcontrol;
+  input [3:0] ALUcontrol;
 
   assign zero = (ALUresult == 0);
 
   @always @ (A or B or ALUcontrol) begin
-
+    case (ALUcontrol)
+      4'b0000: //AND
+        ALUresult = A & B;
+      4'b0001: //OR
+        ALUresult = A | B;
+      4'b0010: //add
+        ALUresult = A + B;
+      4'b0110: //subtract
+        ALUresult = A - B;
+      4'b0111: // set less than
+        ALUresult = (A < B) ? 1'b1 : 1'b0; //ver si esto esta bien, lo hice en la madrugada
+    endcase
   end
