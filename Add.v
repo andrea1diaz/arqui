1d3692c9d9952502e3995174204ed3cd988767b8
Martin <martin-1998@hotmail.com>
2018-11-06 07:01:50 -0500

    Update Add.v
    Añadida opcion para sumas que excedan los 32 bits

--- Add.v
+++ Add.v
@@ -2,7 +2,9 @@ module ADD (A, B, ADDout);
   output [31:0]ADDout;
   input [31:0]A;
   input [31:0]B;
-
-  assign ADDout = A + B;
+  wire[31:0] temp;
+  
+  assign temp = A + B;
+  assign ADDout = temp[31:0];
 
 endmodule
